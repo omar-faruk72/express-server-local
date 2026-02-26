c7a38be4cc8b1152d065b23bcfa3cdd496371245
MD Omar Faruk <mdfaruk01720875872@gmail.com>
2026-02-26 21:51:39 +0600

    added an single todos update api created

--- src/server.ts
+++ src/server.ts
@@ -233,6 +233,31 @@ app.get('/todos/:id', async(req: Request, res: Response) => {
             message: err.message
         })
     }
+});
+app.put('/todos/:id', async(req: Request, res: Response) => {
+    const { user_id, title} = req.body;
+    try{
+        const result = await pool.query(`
+             UPDATE todos SET user_id=$1, title=$2 WHERE id=$3 RETURNING *
+            `, [user_id, title, req.params.id]);
+            if(result.rows.length === 0){
+                res.status(404).json({
+                    success: false,
+                    message: "todos not found"
+                });
+            }else{
+                res.status(200).json({
+                    success: true,
+                    message: "todos successfully update",
+                    data: result.rows[0]
+                })
+            }
+    }catch(err:any){
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
 })
 
 
