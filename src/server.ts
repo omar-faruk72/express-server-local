d5ef29c0f12d24b040d2d6912fcc2f38199bd4ec
AdsFixter Devloper <adsfixterdevloper@AdsFixters-Mac-mini.local>
2026-02-27 11:47:40 +0600

    added an config db.ts file successfully created

--- src/server.ts
+++ src/server.ts
@@ -1,51 +1,12 @@
 import express, { Request, Response } from "express";
-import {Pool} from 'pg';
 import config from "./config";
+import initDB, { pool } from "./config/db";
 
 const app = express();
 const port = config.port;
 
 // parser
 app.use(express.json());
-
-
-
-// DB
-const pool = new Pool({
-    connectionString: `${config.canecxtion_str}`
-});
-
-
-const initDB = async () => {
-  await pool.query(`
-        CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(150) UNIQUE NOT NULL,
-        age INT,
-        phone VARCHAR(15),
-        address TEXT,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-        )
-        `);
-
-       
-  await pool.query(`
-            CREATE TABLE IF NOT EXISTS todos(
-            id SERIAL PRIMARY KEY,
-            user_id INT REFERENCES users(id) ON DELETE CASCADE,
-            title VARCHAR(200) NOT NULL,
-            description TEXT,
-            completed BOOLEAN DEFAULT false,
-            due_date DATE,
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-            )
-            `);
-  };
-
-
 
 initDB();
 
