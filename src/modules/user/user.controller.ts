ea273e8e28d6902390fea5bb2551ccb027b6a0f0
AdsFixter Devloper <adsfixterdevloper@AdsFixters-Mac-mini.local>
2026-02-27 14:31:33 +0600

    added an user contreles creted and successfully post api use

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { pool } from "../../config/db";
+
+ const createUser = async (req: Request, res: Response) => {
+   const {name, email} = req.body;
+
+   try{
+    const result = await pool.query(
+        `INSERT INTO  users(name, email) VALUES($1, $2) RETURNING *`, [name, email]
+    );
+    res.status(201).json({
+        success: true,
+        message: "data insaded fusseccefully",
+        data: result.rows[0]
+    });
+   }
+   catch(err: any) {
+    res.status(500).json({
+        success: false,
+        message: err.message,
+    });
+   }
+};
+
+export const userControllers = {
+    createUser,
+    
+}
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,30 +1,12 @@
 import { Request, Response, Router } from "express";
 import { pool } from "../../config/db";
+import { userControllers } from "./user.controller";
 
 const router = Router();
 
 // localhos:8000/users
-// app.use(/users/)
-router.post('/', async (req: Request, res: Response) => {
-   const {name, email} = req.body;
-
-   try{
-    const result = await pool.query(
-        `INSERT INTO  users(name, email) VALUES($1, $2) RETURNING *`, [name, email]
-    );
-    res.status(201).json({
-        success: true,
-        message: "data insaded fusseccefully",
-        data: result.rows[0]
-    });
-   }
-   catch(err: any) {
-    res.status(500).json({
-        success: false,
-        message: err.message,
-    });
-   }
-});
+// app.use(/users/userRouter)
+router.post('/', userControllers.createUser);
 
 router.get("/", async (req: Request, res:Response) => {
 
